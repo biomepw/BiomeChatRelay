f208838e435347848c4404a96450903fb731cc0d
Jordyn <jordyn@husk.pro>
2020-11-11 14:27:51 +0800

    change to j15

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 version = "1.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_15
+    targetCompatibility = JavaVersion.VERSION_15
 }
 
 repositories {
